2f0101eaceb47167c8f42fd41e6e5eaad32bb966
BsoftMaewada <maewada@live.com>
2023-05-25 06:05:06 -0400

    multiply two numbers

--- ProgrammingJava/src/Variable/Exercise.java
+++ ProgrammingJava/src/Variable/Exercise.java
@@ -1,8 +1,10 @@
 package Variable;
 
+import java.util.Scanner;
+
 public class Exercise {
 
-    //Program to find ASCII code of a character
+    //1. Program to find ASCII code of a character
     public static void main(String[] args) {
         char ch = 'P';
         int asciiCode = ch;
@@ -10,5 +12,19 @@ public class Exercise {
 
         System.out.println("The Ascii value of " + ch + " is: " + asciiCode);
         System.out.println("The Ascii value of " + ch + " is: " + asciiValue);
+
+        //2.  Program to multiply two numbers
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the First Number: ");
+        double m = sc.nextDouble();
+
+        System.out.print("Enter the Second Number: ");
+        double n = sc.nextDouble();
+
+        double num = m*n;
+        System.out.println("The product of the two numbers is: " + num);
+
+
     }
 }
