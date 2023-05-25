c539e6948605d9a2dd71bd53e2ccf17de9efbf79
BsoftMaewada <maewada@live.com>
2023-05-25 04:43:09 -0400

    variable exercise

---
+++ ProgrammingJava/src/Variable/Exercise.java
@@ -0,0 +1,14 @@
+package Variable;
+
+public class Exercise {
+
+    //Program to find ASCII code of a character
+    public static void main(String[] args) {
+        char ch = 'P';
+        int asciiCode = ch;
+        int asciiValue = (int) ch;
+
+        System.out.println("The Ascii value of " + ch + " is: " + asciiCode);
+        System.out.println("The Ascii value of " + ch + " is: " + asciiValue);
+    }
+}
