7f7242457fc3552824a2e05090a29ac0ed206a97
BsoftMaewada <maewada@live.com>
2023-08-18 17:45:43 -0400

    added files

---
+++ ClassExercise/src/TraineeDetails.java
@@ -0,0 +1,11 @@
+public class TraineeDetails {
+
+    public  void display(){
+        System.out.println("My name is Buhari Maiwada");
+        System.out.println("My designation is Software Engineer");
+    }
+    public static void main(String[] args) {
+        TraineeDetails details = new TraineeDetails();
+        details.display();
+    }
+}
